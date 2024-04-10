89cdef01d5b4c74e5d88323541b9ce644cdea353
Ann-Jacqueline <annakaljob@gmail.com>
2024-04-10 15:39:55 +0200

    localhost prüfung

--- src/main/java/backendtech/CityEntry.java
+++ src/main/java/backendtech/CityEntry.java
@@ -1,5 +1,30 @@
 package backendtech;
 
 public class CityEntry {
-    //Test
+    private String city;
+    private String country;
+    private String description;
+    private int temperature;
+    public CityEntry (String city, String country, String description, int temperature) {
+        this.city = city;
+        this.country = country;
+        this.description = description;
+        this.temperature = temperature;
+    }
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
 }
+
--- src/main/java/backendtech/WeatherController.java
+++ src/main/java/backendtech/WeatherController.java
@@ -1,17 +1,17 @@
 package backendtech;
 import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WeatherController {
 
-
         @GetMapping("/greeting")
-        public CityEntry greeting () {
-            return new CityEntry();
+        public CityEntry greeting (@PathVariable String city, @PathVariable String country, @PathVariable String description, @PathVariable String temperature) {
+                CityEntry weather = new CityEntry ("Berlin", "Germany " , "Sunny", 25);
+                return greeting(city,country,description,temperature);
         }
 
 }
