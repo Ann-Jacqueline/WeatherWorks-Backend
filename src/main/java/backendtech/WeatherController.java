5a833d11d0363876f6bc2fe685aaa0bb7dd97652
Ann-Jacqueline <annakaljob@gmail.com>
2024-04-10 13:26:26 +0200

    localhost test

--- src/main/java/backendtech/WeatherController.java
+++ src/main/java/backendtech/WeatherController.java
@@ -1,5 +1,17 @@
 package backendtech;
+import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class WeatherController {
+
+
+        @GetMapping("/greeting")
+        public CityEntry greeting () {
+            return new CityEntry();
+        }
 
 }
