da2a1ffa9be222876e48bebc393b350fce151ef2
Ann-Jacqueline <annakaljob@gmail.com>
2024-04-16 14:50:16 +0200

    Milestone 1 fertig

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # backend-webtech
+## Thema: Wetter App
+## Die Wetter App soll externe Daten ziehen(durch externe APIs) und User Input speichern und verwalten
+## 7 tasks/ Use Cases: 
+### 1: Eine Übersicht einer Stadt (externe Daten ziehen)
+### 2: User Input speichern durch: Möchte ich hin (searching funktion)
+### 3: Favoriten Liste
+### 4: Reisepläne Liste
+### 5: Standorte verwalten
+### 6:(Standorte verwalten): 1: Standorte Löschen
+### 7: Einstellungen (Einheit °C oder °F)
--- src/main/java/backendtech/CityEntry.java
+++ src/main/java/backendtech/CityEntry.java
@@ -1,4 +1,47 @@
 package backendtech;
 
 public class CityEntry {
+    private String name;
+    private double längengrad;
+    private double breitengrad;
+    private boolean isDeleted;
+
+    public CityEntry(String name, double längengrad, double breitengrad, boolean isDeleted) {
+        this.name = name;
+        this.längengrad = längengrad;
+        this.breitengrad = breitengrad;
+        this.isDeleted = isDeleted;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLängengrad() {
+        return längengrad;
+    }
+
+    public void setLängengrad(double längengrad) {
+        this.längengrad = längengrad;
+    }
+
+    public double getBreitengrad() {
+        return breitengrad;
+    }
+
+    public void setBreitengrad(double breitengrad) {
+        this.breitengrad = breitengrad;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
 }
