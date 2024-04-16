785936d155bef2c5da4943e228e888ea3711a239
Ann-Jacqueline <annakaljob@gmail.com>
2024-04-16 14:50:16 +0200

    Milestone 1 fertig

--- src/main/java/backendtech/WeatherController.java
+++ src/main/java/backendtech/WeatherController.java
@@ -2,11 +2,17 @@ package backendtech;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class WeatherController{
         @GetMapping("/greeting")
-        public String greeting() {
-                return "Application works!";
+        public List<CityEntry> greeting() {
+                CityEntry city = new CityEntry("Berlin",52.5162, 13.3777, false);
+                CityEntry city1 = new CityEntry("Paris",2.3522, 48.8566, false);
+                CityEntry city2 = new CityEntry("Madrid",-3.6919, 40.4189, false);
+                return List.of(city,city1,city2);
+
         }
 
 }
