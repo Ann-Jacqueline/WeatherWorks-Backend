38cb64ea87bfb1696a2ffac94f1c0d5902425d8e
Ann-Jacqueline <annakaljob@gmail.com>
2024-07-01 14:45:43 +0200

    Deployment Authoursation V3

--- src/main/java/backendtech/web/CityHistoryOwnerController.java
+++ src/main/java/backendtech/web/CityHistoryOwnerController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+
 
 @Controller
 @AllArgsConstructor
@@ -26,21 +26,19 @@ public class CityHistoryOwnerController {
     private final CityHistoryOwnerService cityHistoryOwnerService;
     private static final Logger logger = LoggerFactory.getLogger(CityHistoryOwnerController.class);
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Iterable<CityHistoryOwner>> getCityHistoryOwners(HttpServletRequest request) {
-        if (request.getSession(false) == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    @PostMapping("/login")
+    public ResponseEntity<Void> loginUser(@RequestBody Map<String, String> userData, HttpServletRequest request) {
+        String userName = userData.get("userName");
+        if (userName != null && !userName.isEmpty()) {
+            HttpSession session = request.getSession(true);
+            session.setAttribute("userName", userName);
+            logger.info("New session started, Session ID: {}", session.getId());
+            logger.info("Username '{}' added to session.", userName);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } else {
+            logger.warn("Login attempt with invalid or empty user data.");
+            return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(cityHistoryOwnerService.getCityHistoryOwners());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<CityHistoryOwner> getCityHistoryOwner(@PathVariable("id") final Long id, HttpServletRequest request) {
-        if (request.getSession(false) == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        }
-        final Optional<CityHistoryOwner> found = cityHistoryOwnerService.getCityHistoryOwner(id);
-        return found.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/current")
@@ -60,36 +58,30 @@ public class CityHistoryOwnerController {
         }
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        if (request.getSession(false) != null) {
+            request.getSession().invalidate();
+            logger.info("Session invalidated.");
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Iterable<CityHistoryOwner>> getCityHistoryOwners(HttpServletRequest request) {
+        if (request.getSession(false) == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok(cityHistoryOwnerService.getCityHistoryOwners());
+    }
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<CityHistoryOwner> addCityHistoryOwner(@Valid @RequestBody CityHistoryOwner body, HttpServletRequest request) {
         if (request.getSession(false) != null) {
             final CityHistoryOwner newCityHistoryOwner = new CityHistoryOwner(body.getUserName());
             final CityHistoryOwner addedCityHistoryOwner = cityHistoryOwnerService.addCityHistoryOwner(newCityHistoryOwner);
             return new ResponseEntity<>(addedCityHistoryOwner, HttpStatus.CREATED);
-        }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<Void> loginUser(@RequestBody Map<String, String> userData, HttpServletRequest request) {
-        String userName = userData.get("userName");
-        if (userName != null && !userName.isEmpty()) {
-            HttpSession session = request.getSession(true);
-            session.setAttribute("userName", userName);
-            logger.info("New session started, Session ID: {}", session.getId());
-            logger.info("Username '{}' added to session.", userName);
-            return ResponseEntity.status(HttpStatus.CREATED).build();
-        } else {
-            logger.warn("Login attempt with invalid or empty user data.");
-            return ResponseEntity.badRequest().build();
-        }
-    }
-    //last current username schreiben
-    @PostMapping("/logout")
-    public ResponseEntity<Void> logout(HttpServletRequest request) {
-        if (request.getSession(false) != null) {
-            request.getSession().invalidate();
-            return ResponseEntity.ok().build();
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
